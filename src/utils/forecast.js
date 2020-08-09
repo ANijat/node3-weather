3d7a3e22a40dfe8c24ac90b8bc120bd9cdab5d44
root <aslanovnicat@gmail.com>
2020-08-09 08:54:43 +0400

    feels like added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ request({url, json: true}, (error, { body })=>{
     callback('Unable find location', undefined)
 }
  else{
-   callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' out. There is a ' + body.current.precip + '% chance of rain!')
+   callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' out, but feels like ' + body.current.feelslike + '. There is a ' + body.current.precip + '% chance of rain! ')
 }
 })
 }
